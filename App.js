8594ae578b83071d911a5028b12f72dcdec6117c
Joel Silva <henriquesa@ipvc.pt>
2022-02-11 15:16:33 +0000

    state

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text,  TouchableOpacity,  View } from 'react-native';
+import React, { Component } from 'react'
 
 
 const Greeting = (props) => {
@@ -11,7 +12,17 @@ const Greeting = (props) => {
 }
 
 
-export default function App() {
+class App extends Component {
+  state = {
+    count: 0
+  }
+
+  onPress = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+  render() {
   return (
     <View style={styles.container}>
       <Greeting name ="Henrique"/>
@@ -19,8 +30,20 @@ export default function App() {
       <Greeting name ="Lima"/>
       <Greeting name ="Silva"/>
       <StatusBar style="auto" />
+      <TouchableOpacity
+         style={styles.button}
+         onPress={this.onPress}
+        >
+         <Text>Click me</Text>
+        </TouchableOpacity>
+        <View>
+          <Text>
+            You clicked { this.state.count } times
+          </Text>
+        </View>
     </View>
   );
+}
 }
 
 const styles = StyleSheet.create({
@@ -36,5 +59,13 @@ const styles = StyleSheet.create({
     width:'50%',
     margin:5,
     backgroundColor:'#f00'
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    margin: 10
   }
-});
+})
+
+export default App;
