0faa4aeec26ffc225084f9a8af0b609956c27bcf
Joel Silva <henriquesa@ipvc.pt>
2022-02-14 18:44:09 +0000

    flex

--- App.js
+++ App.js
@@ -1,86 +1,107 @@
-import { StatusBar } from 'expo-status-bar';
-import { View, Text, Image, ScrollView, TextInput,StyleSheet, Button } from 'react-native';
-import React, { Component } from 'react'
+import React, { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
+const FlexDirectionBasics = () => {
+  const [flexDirection, setflexDirection] = useState("column");
 
-
-
-class App extends Component {
-  state = { isHungry: true };
-  render(){
   return (
-    <ScrollView style={styles.container} >
-      {/* <StatusBar style="auto" /> */}
-    <View >
-      <Text> Cat name: {this.props.name}</Text>
-      <Image
-        source={{
-          uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
-        }}
-        style={{ width: 200, height: 200 }}
+    <PreviewLayout
+      label="flexDirection"
+      values={["column", "row", "row-reverse", "column-reverse"]}
+      selectedValue={flexDirection}
+      setSelectedValue={setflexDirection}
+    >
+      <View
+        style={[styles.box, { backgroundColor: "powderblue" }]}
       />
+      <View
+        style={[styles.box, { backgroundColor: "skyblue" }]}
+      />
+      <View
+        style={[styles.box, { backgroundColor: "steelblue" }]}
+      />
+    </PreviewLayout>
+  );
+};
+
+const PreviewLayout = ({
+  label,
+  children,
+  values,
+  selectedValue,
+  setSelectedValue,
+}) => (
+  <View style={{ padding: 10, flex: 1 }}>
+    <Text style={styles.label}>{label}</Text>
+    <View style={styles.row}>
+      {values.map((value) => (
+        <TouchableOpacity
+          key={value}
+          onPress={() => setSelectedValue(value)}
+          style={[
+            styles.button,
+            selectedValue === value && styles.selected,
+          ]}
+        >
+          <Text
+            style={[
+              styles.buttonLabel,
+              selectedValue === value && styles.selectedLabel,
+            ]}
+          >
+            {value}
+          </Text>
+        </TouchableOpacity>
+      ))}
     </View>
-    <TextInput
-      style={styles.input}
-      defaultValue="You can type in me"
-    />
-    
-    <Text style={{marginTop:15,marginBottom:15}}>
-          I am {this.props.name}, and I am
-          {this.state.isHungry ? " hungry" : " full"}!
-        </Text>
-        <Button  style={styles.button}
-          onPress={() => {
-            this.setState({ isHungry: false });
-          }}
-          disabled={!this.state.isHungry}
-          title={
-            this.state.isHungry ? "Pour me some milk, please!" : "Thank you!"
-          }
-        />
-  </ScrollView>
-
-
+    <View style={[styles.container, { [label]: selectedValue }]}>
+      {children}
+    </View>
+  </View>
 );
-}
-}
-class Cafe extends Component {
-  render() {
-    return (
-      <>
-        <App name="Munkustrap"/>
-        <App name="Spot" />
-      </>
-    );
-  }
-}
-
-
-
 
 const styles = StyleSheet.create({
-  container:{
-    marginBottom:10,
-    marginTop:15,
-    backgroundColor:'#AAF',   
-    margin:5, 
-    padding:15,
-    borderRadius:4,
-    borderColor:'gray',
-    borderWidth:3,
-   
-    
+  container: {
+    flex: 1,
+    marginTop: 8,
+    backgroundColor: "aliceblue",
   },
-  input:{
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    padding:5,
-    backgroundColor:'#FFF',
-    borderRadius:4,
-    paddingLeft:15
-  }
-})
+  box: {
+    width: 50,
+    height: 50,
+  },
+  row: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  button: {
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+    borderRadius: 4,
+    backgroundColor: "oldlace",
+    alignSelf: "flex-start",
+    marginHorizontal: "1%",
+    marginBottom: 6,
+    minWidth: "48%",
+    textAlign: "center",
+  },
+  selected: {
+    backgroundColor: "coral",
+    borderWidth: 0,
+  },
+  buttonLabel: {
+    fontSize: 12,
+    fontWeight: "500",
+    color: "coral",
+  },
+  selectedLabel: {
+    color: "white",
+  },
+  label: {
+    textAlign: "center",
+    marginBottom: 10,
+    fontSize: 24,
+  },
+});
 
-
-export default Cafe;
+export default FlexDirectionBasics;
