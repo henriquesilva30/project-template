d25617c222b6be22f28a8b1b88bc54bed4178fee
Joel Silva <henriquesa@ipvc.pt>
2022-02-21 22:59:22 +0000

    dim

--- app/components/WelcomeScreen.js
+++ app/components/WelcomeScreen.js
@@ -1,14 +1,50 @@
-import React from 'react';
-import { View, ImageBackground, StyleSheet, Image, Text, Pressable} from 'react-native';
+import React , {useState} from 'react';
+import { View, ImageBackground, StyleSheet, Image, Text, Pressable, Modal, TextInput} from 'react-native';
 import colors from '../config/colors';
 
 function WelcomeScreen({ navigation}) {
-    return (
+  const [modalVisible, setModalVisible] = useState(false);
+  const  [text,setText] = useState('');
+
+  const onChange = textValue => setText(textValue);
+
+  return (
         <ImageBackground style={styles.background} source={require('../assets/bg.jpg')}>
             <View style={styles.bglogo}>
             </View>
             <Image style={styles.logo} source={require('../assets/logo.jpg')} />
-            <Pressable style={styles.loginBtn}>
+            <Modal
+          animationType="fade"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            Alert.alert("Modal has been closed.");
+            setModalVisible(!modalVisible);
+          }}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalText}>Login \  
+              <Text style={styles.modalText}> Registar</Text></Text>
+              <TextInput placeholder="Adicionar artigo" onChangeText={onChange} value={text} style={styles.input}/>
+              <TextInput placeholder="Adicionar artigo" onChangeText={onChange} value={text} style={styles.input}/>
+
+              <Pressable
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => setModalVisible(!modalVisible)}
+              >
+                <Text style={styles.textStyle}>Cancelar</Text>
+              </Pressable>
+              <Pressable
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => setModalVisible(!modalVisible)}
+              >
+                <Text style={styles.textStyle}>Cancelar</Text>
+              </Pressable>
+            </View>
+          </View>
+        </Modal>
+            <Pressable style={styles.loginBtn}  onPress={() => setModalVisible(true)}>
                 <Text style={styles.txt} >Aceder à conta</Text> 
             </Pressable>
             <Pressable style={styles.regBtn}  onPress={() => navigation.navigate('Home')} >
@@ -22,7 +58,19 @@ const styles = StyleSheet.create({
     background:{
         flex:1,
         justifyContent:'flex-end',
+
     },
+    input:{
+      height:40,
+      padding:10,
+      margin:5,
+      marginTop:25,
+      borderRadius:15,
+      fontSize:16,
+      borderWidth:2,
+      backgroundColor:colors.white,
+      color:colors.black
+  },
     loginBtn:{
         width:'100%',
         height:70,
@@ -34,31 +82,76 @@ const styles = StyleSheet.create({
         backgroundColor:colors.secondary
     },
     bglogo:{
-        width:250,
-        height:250,
-        justifyContent:'center',
-        backgroundColor:colors.white,
-        alignSelf:'center',
-        opacity:0.4,
-        bottom:'40%',
-        borderRadius:200,
+      width:250,
+      height:250,
+      justifyContent:'center',
+      backgroundColor:colors.white,
+      alignSelf:'center',
+      opacity:0.4,
+      position:'absolute',
+      bottom:'50%',
+      borderRadius:200,
     },
     logo : {
         width:200,
         height:200,
         alignSelf:'center',
-        position:'absolute',
-        bottom:'61%',
         borderRadius:200,
         borderWidth:1,
+        position:'absolute',
+        bottom:'50%',
+        marginBottom:25,
         borderColor:colors.black,
-
     },
     txt: {
         alignSelf:'center',
         color:colors.white,
         fontSize:18,
         top:'35%',
+    },
+    centeredView: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: 22
+    },
+    modalView: {
+      height:300,
+      width:300,
+      opacity:1,
+      margin: 20,
+      backgroundColor: colors.white,
+      borderRadius: 20,
+      padding: 35,
+      alignItems: "center",
+      shadowColor: colors.secondary,
+      shadowOffset: {
+        width: 1,
+        height: 2
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 4,
+      elevation: 5
+    },
+    button: {
+      borderRadius: 20,
+      padding: 10,
+      elevation: 2
+    },
+    buttonOpen: {
+      backgroundColor: 'green',
+    },
+    buttonClose: {
+      backgroundColor: 'red',
+    },
+    textStyle: {
+      color: "white",
+      fontWeight: "bold",
+      textAlign: "center"
+    },
+    modalText: {
+      marginBottom: 15,
+      textAlign: "center"
     }
 })
 
